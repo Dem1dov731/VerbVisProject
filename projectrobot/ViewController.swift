304f7822de1fb33a6687a1d65392c7b2937c199a
Dem1dov731 <grishan1018@gmail.com>
2023-02-19 10:13:55 +0300

    Initial Commit

---
+++ projectrobot/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  projectrobot
+//
+//  Created by Grigory Demidov on 19.02.2023.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
