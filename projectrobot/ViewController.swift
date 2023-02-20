1e0ab5491e4f42ab121d6ff6b414e7a06c0571c6
Dem1dov731 <grishan1018@gmail.com>
2023-02-20 09:41:39 +0300

    base button

--- projectrobot/ViewController.swift
+++ projectrobot/ViewController.swift
@@ -3,45 +3,44 @@ import RealityKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    @IBOutlet var arView: ARView! // отображает сцену AR на экране
     
-    var anchorMan: ManPuppet.SceneBase!
-    var isBaseButtonTapped = false
+    var anchorMan: ManPuppet.SceneBase! // базовая модель, созданная в Reality Composer
+    var isBaseButtonTapped = false // флаг для отслеживания нажатия на кнопку Base
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Не загружаем базовую модель здесь
+    override func viewDidLoad() { // вызывается при загрузке представления
+        super.viewDidLoad() // вызов родительского метода
     }
     
-    @IBAction func WalkOverTapped(_ sender: Any) {
-        anchorMan.notifications.walkOver.post()
+    @IBAction func WalkOverTapped(_ sender: Any) { // метод вызывается при нажатии кнопки WalkOver
+        anchorMan.notifications.walkOver.post() // вызывает действие walkOver модели anchorMan
     }
     
-    @IBAction func PickUpTapped(_ sender: Any) {
-        anchorMan.notifications.pickUpReaction.post()
+    @IBAction func PickUpTapped(_ sender: Any) { // метод вызывается при нажатии кнопки PickUp
+        anchorMan.notifications.pickUpReaction.post() // вызывает действие pickUpReaction модели anchorMan
     }
     
-    @IBAction func StandUpTapped(_ sender: Any) {
-        anchorMan.notifications.standUpReact.post()
+    @IBAction func StandUpTapped(_ sender: Any) { // метод вызывается при нажатии кнопки StandUp
+        anchorMan.notifications.standUpReact.post() // вызывает действие standUpReact модели anchorMan
     }
     
-    @IBAction func SitDownTapped(_ sender: Any) {
-        anchorMan.notifications.sitDownReaction.post()
+    @IBAction func SitDownTapped(_ sender: Any) { // метод вызывается при нажатии кнопки SitDown
+        anchorMan.notifications.sitDownReaction.post() // вызывает действие sitDownReaction модели anchorMan
     }
     
-    @IBAction func LookOverTapped(_ sender: Any) {
-        anchorMan.notifications.lookOverReaction.post()
+    @IBAction func LookOverTapped(_ sender: Any) { // метод вызывается при нажатии кнопки LookOver
+        anchorMan.notifications.lookOverReaction.post() // вызывает действие lookOverReaction модели anchorMan
     }
     
-    @IBAction func BaseTapped(_ sender: Any) {
-        if !isBaseButtonTapped {
-            anchorMan = try! ManPuppet.loadSceneBase()
-            anchorMan.generateCollisionShapes(recursive: true)
-            arView.scene.anchors.append(anchorMan)
-            isBaseButtonTapped = true
-            let button = sender as! UIButton
-            button.setTitle("Base loaded", for: .normal)
+    @IBAction func BaseTapped(_ sender: Any) { // метод вызывается при нажатии кнопки Base
+        if !isBaseButtonTapped { // если флаг равен false
+            anchorMan = try! ManPuppet.loadSceneBase() // загрузка базовой модели из Reality Composer
+            anchorMan.generateCollisionShapes(recursive: true) // создание коллизий для модели
+            arView.scene.anchors.append(anchorMan) // добавление модели на сцену AR
+            isBaseButtonTapped = true // установка флага в true
+            let button = sender as! UIButton // приведение отправителя к типу UIButton
+            button.setTitle("Base loaded", for: .normal) // изменение заголовка кнопки на "Base loaded"
         }
-        anchorMan.notifications.baseReact.post()
+        anchorMan.notifications.baseReact.post() // вызывает действие baseReact модели anchorMan
     }
 }
